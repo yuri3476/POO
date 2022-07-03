6efded101357233ffa55820fbc44d1c46e0be237
Yuri Rezende <83989931+yuri3476@users.noreply.github.com>
2022-07-03 19:46:58 -0300

    Add files via upload

---
+++ Lógica de Programação/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Primeiro_Programa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Word!");
+        }
+    }
+}
